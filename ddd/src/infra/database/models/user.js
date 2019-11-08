d54e406a8f6577adbbf5972f62f5b5b4703a0fcb
erobit <erobit3@gmail.com>
2019-11-07 23:35:15 -0500

    updated domain model to support injection of db

--- ddd/src/infra/database/models/user.js
+++ ddd/src/infra/database/models/user.js
@@ -2,64 +2,77 @@ const ObjectID = require('mongodb').ObjectID;
 const { encryptPassword } = require('../../encryption');
 const Email = require('../../support/email');
 
-const create = async (collection, user) => {
-  try {
-    user.password = encryptPassword(user.password);
-    const results = await collection.insertOne(user);
-    return results.ops[0];
-  } catch (e) {
-    throw e;
-  }
-};
+module.exports = ({ db }) => {
+  const create = async user => {
+    try {
+      user.password = encryptPassword(user.password);
+      const results = await db.collection('users').insertOne(user);
+      return results.ops[0];
+    } catch (e) {
+      throw e;
+    }
+  };
 
-const getAll = async collection => {
-  try {
-    const results = await collection.find().toArray();
-    return results;
-  } catch (e) {
-    throw e;
-  }
-};
+  const getAll = async () => {
+    try {
+      const results = await db
+        .collection('users')
+        .find()
+        .toArray();
+      return results;
+    } catch (e) {
+      throw e;
+    }
+  };
 
-const findById = async (collection, id) => {
-  try {
-    if (!ObjectID.isValid(id)) throw 'Invalid MongoDB ID.';
-    const results = await collection.findOne(ObjectID(id));
-    return results;
-  } catch (e) {
-    throw e;
-  }
-};
+  const findById = async id => {
+    try {
+      if (!ObjectID.isValid(id)) throw 'Invalid MongoDB ID.';
+      const results = await db.collection('users').findOne(ObjectID(id));
+      return results;
+    } catch (e) {
+      throw e;
+    }
+  };
 
-const findOne = async (collection, query) => {
-  try {
-    const results = await collection.findOne(query);
-    return results;
-  } catch (e) {
-    throw e;
-  }
-};
+  const findOne = async query => {
+    try {
+      const results = await db.collection('users').findOne(query);
+      return results;
+    } catch (e) {
+      throw e;
+    }
+  };
 
-const findByEmail = async (collection, email) => {
-  try {
-    if (!Email.isValid(email)) throw 'Invalid email address';
-    const results = await collection.findOne({ email: { $in: [email] } });
-    return results;
-  } catch (e) {
-    throw e;
-  }
-};
+  const findByEmail = async email => {
+    try {
+      if (!Email.isValid(email)) throw 'Invalid email address';
+      const results = await db
+        .collection('users')
+        .findOne({ email: { $in: [email] } });
+      return results;
+    } catch (e) {
+      throw e;
+    }
+  };
 
-const update = async (collection, userId, fields) => {
-  try {
-    const results = await collection.updateOne(
-      { _id: { $eq: userId } },
-      { $set: fields }
-    );
-    return results.ops[0];
-  } catch (e) {
-    throw e;
-  }
-};
+  const update = async (userId, fields) => {
+    try {
+      const results = await db
+        .collection('users')
+        .updateOne({ _id: { $eq: userId } }, { $set: fields });
+      return results.ops[0];
+    } catch (e) {
+      throw e;
+    }
+  };
 
-module.exports = { create, getAll, findById, findOne, findByEmail, update };
+  return {
+    create,
+    getAll,
+    findById,
+    findOne,
+    findByEmail,
+    update
+  };
+};
--- ddd/src/infra/repositories/index.js
+++ ddd/src/infra/repositories/index.js
@@ -1,10 +1,9 @@
 const User = require('./user');
 
 module.exports = ({ database }) => {
-  console.log(database);
   const userModel = database.mongo.models.users;
 
   return {
-    userRepository: User({ model: userModel, db: database.mongo.db })
+    userRepository: User({ model: userModel({ db: database }) })
   };
 };
