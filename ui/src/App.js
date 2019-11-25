3c8946df223c5c9a01f42d3e7bf3818a01b9b66a
Trevor Delamorandiere <tdelam@gmail.com>
2019-11-24 19:25:29 -0500

    Added the dashboard and wired up to make sure we're logged in

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import AuthService from './services/AuthService';
 import withAuth from './components/withAuth';
+import Dashboard from './components/Dashboard';
 
 const Auth = new AuthService();
 
@@ -15,17 +16,161 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome {this.props.user.username}</h2>
-        </div>
-        <p className="App-intro">
-          <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
-          <a href="/dashboard">Dashboard</a>
-        </p>
-        
-      </div>
+      <React.Fragment>
+        {
+          Auth.loggedIn() &&
+          <div className="app" id="app">
+            <div id="aside" className="app-aside fade box-shadow-x nav-expand white" aria-hidden="true">
+              <div className="sidenav modal-dialog dk white">
+                <div className="navbar lt">
+                  <a href="index.html" className="navbar-brand">
+                    <span className="hidden-folded d-inline">
+                      <img src="../assets/images/mcmaster-logo.png" alt="." className="logo" />
+                    </span>
+                  </a>
+                </div>
+                <div className="flex hide-scroll">
+                  <div className="scroll">
+                    <div className="nav-border b-primary" data-nav>
+                      <ul className="nav bg">
+                        <li className="nav-header">
+                          <div className="py-3">
+                            <a href="#" className="btn btn-sm success theme-accent btn-block">
+                              <i className="fa fa-fw fa-plus"></i>
+                              <span className="hidden-folded d-inline">New Project</span>
+                            </a>
+                          </div>
+                          <span className="text-xs hidden-folded">Main</span>
+                        </li>
+                        <li>
+                          <a href="dashboard.html">
+                            <span className="nav-icon"><i className="fa fa-dashboard"></i></span>
+                            <span className="nav-text">Dashboard</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a>
+                            <span className="nav-caret"><i className="fa fa-caret-down"></i></span>
+                            <span className="nav-icon"><i className="fa fa-align-left"></i></span>
+                            <span className="nav-text">Layout</span>
+                          </a>
+                          <ul className="nav-sub">
+                            <li>
+                              <a href="layout.header.html">
+                                <span className="nav-text">Header</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="layout.sidenav.html">
+                                <span className="nav-text">Sidenav</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="layout.aside.html">
+                                <span className="nav-text">Aside</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="layout.flex.html">
+                                <span className="nav-text">Flex</span>
+                              </a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a href="app.inbox.html">
+                            <span className="nav-badge"><b className="badge badge-sm badge-pill success">6</b></span>
+                            <span className="nav-icon"><i className="fa fa-envelope"></i></span>
+                            <span className="nav-text">Inbox</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="app.message.html">
+                            <span className="nav-icon"><i className="fa fa-comment"></i></span>
+                            <span className="nav-text">Messages</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="app.user.html">
+                            <span className="nav-icon"><i className="fa fa-phone-square"></i></span>
+                            <span className="nav-text">Users</span>
+                          </a>
+                        </li>
+                        <li className="pb-2 hidden-folded"></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="content" className="app-content box-shadow-0" role="main">
+              <div className="content-header white  box-shadow-0" id="content-header">
+                <div className="navbar navbar-expand-lg">
+                  <a className="d-lg-none mx-2" data-toggle="modal" data-target="#aside">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+                      <path d="M80 304h352v16H80zM80 248h352v16H80zM80 192h352v16H80z" />
+                    </svg>
+                  </a>
+                  <div className="navbar-text nav-title flex" id="pageTitle">Blank</div>
+                  <ul className="nav flex-row order-lg-2">
+                    <li className="dropdown d-flex align-items-center">
+                      <a href="#" data-toggle="dropdown" className="d-flex align-items-center">
+                        <span className="avatar w-32">
+                          <img src="../assets/images/a3.jpg" alt="..." />
+                        </span>
+                      </a>
+                      <div className="dropdown-menu dropdown-menu-right w pt-0 mt-2 animate fadeIn">
+                        <a className="dropdown-item" href="profile.html">
+                          <span>Profile</span>
+                        </a>
+                        <a className="dropdown-item" href="setting.html">
+                          <span>Settings</span>
+                        </a>
+                        <a className="dropdown-item" href="app.inbox.html">
+                          <span className="float-right"><span className="badge info">6</span></span>
+                          <span>Inbox</span>
+                        </a>
+                        <a className="dropdown-item" href="app.message.html">
+                          <span>Message</span>
+                        </a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="docs.html">
+                          Need help?
+                        </a>
+                        <a className="dropdown-item" href="signin.html">Sign out</a>
+                      </div>
+                    </li>
+                    <li className="d-lg-none d-flex align-items-center">
+                      <a href="#" className="mx-2" data-toggle="collapse" data-target="#navbarToggler">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 512 512">
+                          <path d="M64 144h384v32H64zM64 240h384v32H64zM64 336h384v32H64z" />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="collapse navbar-collapse no-grow order-lg-1" id="navbarToggler">
+                    <form className="input-group m-2 my-lg-0">
+                      <span className="input-group-btn">
+                        <button type="button" className="btn no-border no-bg no-shadow"><i className="fa fa-search"></i></button>
+                      </span>
+                      <input type="text" className="form-control no-border no-bg no-shadow" placeholder="Search projects..." />
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="content-main " id="content-main">
+                <Dashboard />
+              </div>
+
+              <div className="content-footer white " id="content-footer">
+                <div className="d-flex p-3">
+                  <span className="text-sm text-muted flex">&copy; Copyright. McMaster University</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </React.Fragment>
     );
   }
 }
