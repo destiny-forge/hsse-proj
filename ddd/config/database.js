833de4d56ca6d1e8113a8e309ca312bba331aa55
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-04 23:51:30 -0500

    turned off useUnifiedTopology

--- ddd/config/database.js
+++ ddd/config/database.js
@@ -29,7 +29,7 @@ module.exports = {
     name: "hsse_staging",
     options: {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: false
     }
   },
   production: {
