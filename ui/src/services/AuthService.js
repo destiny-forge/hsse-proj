02abcd3b3d2d1b0a33c6a76bd9e64493295f0adc
erobit <erobit3@gmail.com>
2019-12-05 22:25:54 -0500

    werk

--- ui/src/services/AuthService.js
+++ ui/src/services/AuthService.js
@@ -3,7 +3,7 @@ import decode from 'jwt-decode';
 class AuthService {
   // Initializing important variables
   constructor(domain) {
-    this.domain = domain || 'http://localhost:5001';
+    this.domain = domain;
     this.fetch = this.fetch.bind(this);
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
