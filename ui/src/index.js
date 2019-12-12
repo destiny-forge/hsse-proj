2f6548caf6e50a630fb40d8867164314c5c8f989
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-12 00:29:46 -0500

    Finished moving all routes into their respective HOC's

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,31 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './components/Accounts/Login';
-import Dashboard from './components/Dashboard';
-import Signup from './components/Accounts/Signup';
-import ArticleCreate from './components/Articles/Create';
-import SignupSuccess from './components/Accounts/SignupSuccess';
-import ConfirmEmail from './components/Accounts/ConfirmEmail';
-import ForgotPassword from './components/Accounts/ForgotPassword';
-import Assigned from './components/HealthSystemsAssigned/Assigned';
-import PasswordReset from './components/Accounts/PasswordReset';
+
 
 ReactDOM.render(
-  <Router>
-    <Route exact path="/" component={App} />
-    <Route path="/login" component={Login} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/forgot-password" component={ForgotPassword} />
-    <Route path="/account/reset/:token" component={PasswordReset} />
-    <Route path="/signup-success" component={SignupSuccess} />
-    <Route path="/account/confirm/:token" component={ConfirmEmail} />
-    <Route path="/dashboard" component={Dashboard} />
-    <Route path="/article" component={ArticleCreate} />
-    <Route path="/health-systems-assigned" component={Assigned} />
-  </Router>,
-  document.getElementById('app')
+  <App />, document.getElementById('app')
 );
 
 // If you want your app to work offline and load faster, you can change
