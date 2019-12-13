698604ee1f38751654da4e35bf155411d89b4cc4
Trevor Delamorandiere <tdelam@gmail.com>
2019-12-12 23:32:34 -0500

    * Modified the ddd list method. It was expecting type as an arg. Since this will be tied to a profile or role, I removed this as an arg for now and put a TODO so that we can readdress that when we tie it to a role or user * Updated the nav and removed the Assigned component as it is not needed

--- ddd/src/app/article/list.js
+++ ddd/src/app/article/list.js
@@ -4,12 +4,15 @@
 module.exports = ({ articleRepository }) => {
   const list = async type => {
     try {
-      if (!type || (type !== "sse" && type !== "hse")) {
-        return {
-          error: "A valid article type is required"
-        };
-      }
-      return await articleRepository.findByType(type);
+      // TODO: change this to look up on profile, for now let's get all
+      
+      // if (!type || (type !== "sse" && type !== "hse")) {
+      //   return {
+      //     error: "A valid article type is required"
+      //   };
+      // }
+      // return await articleRepository.findByType(type);
+      return await articleRepository.getAll();
     } catch (error) {
       throw new Error(error);
     }
