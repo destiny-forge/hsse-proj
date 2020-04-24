6b0c69f49a4dd13680819c5bc3c0c0f8b9f8c816
erobit <erobit3@gmail.com>
2020-04-24 14:37:43 -0400

    fix

--- ddd/src/app/article/assign.js
+++ ddd/src/app/article/assign.js
@@ -4,25 +4,25 @@ const _ = require("underscore");
  * Article assignment
  */
 module.exports = ({ articleRepository }) => {
-  const assign = async assignment => {
+  const assign = async (assignment) => {
     try {
-      const { articleId, stage, type, user } = assignment;
-      
+      const { articleId, stage, type, userId } = assignment;
+
       if (!type || (type !== "junior" && type !== "senior")) {
         return {
-          error: "A valid assignment type is required"
+          error: "A valid assignment type is required",
         };
       }
 
       if (!articleId) {
         return {
-          error: "A valid article _id is required"
+          error: "A valid article _id is required",
         };
       }
 
-      if (!user._id) {
+      if (!userId) {
         return {
-          error: "A valid user _id is required"
+          error: "A valid user _id is required",
         };
       }
 
@@ -35,7 +35,7 @@ module.exports = ({ articleRepository }) => {
           stage !== "translations")
       ) {
         return {
-          error: "A valid assignment stage is required"
+          error: "A valid assignment stage is required",
         };
       }
 
@@ -60,6 +60,6 @@ module.exports = ({ articleRepository }) => {
   };
 
   return {
-    assign
+    assign,
   };
 };
