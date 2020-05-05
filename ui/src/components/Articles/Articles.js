3fa36ed9e2df80d5fff4e2fa8868d6a91d9ce461
Trevor Delamorandiere <tdelam@gmail.com>
2020-05-04 22:17:53 -0400

    Removed useless modal and got rid of reload page

--- ui/src/components/Articles/Articles.js
+++ ui/src/components/Articles/Articles.js
@@ -1,84 +1,44 @@
+import _ from 'lodash';
 import React from 'react';
-import Modal from '../Eligibility/Modal';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import ArticleService from '../../services/ArticleService';
 import withAuth from '../withAuth';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
-import Assignment from './Assignment';
 
 class Articles extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      articles: [],
-      showJunior: false,
-      showSenior: false 
-    }
+    this.state = { articles: [] }
     this.Article = ArticleService({ fetch: this.props.fetch });
-  }
-
-  showModal = (type) => {
-    this.setState({
-      [`show${type}`]: true
-    });
-  }
-
-  toTitleCase = (str) => {
-    return str.replace(
-      /\w\S*/g,
-      (txt) => {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      }
-    );
   }
 
   notifyDone = () => toast.success("Assignment created successfully.");
 
-  hideModal = (user, stage, articleId, type) => {
-    if (user && stage && articleId && type) {
-      const assignment = {
-        articleId,
-        stage,
-        type,
-      };
+  showEmail = (article, type) => {
+    const { eligibility } = article.stages;
 
-      assignment.user = {
-        _id: user.id,
-        email: user.email
+    if (eligibility.status === 'half_assigned') {
+      if (!_.isUndefined(eligibility[type])) {
+        return eligibility[type].email;
       }
-      this.Article.assign(assignment).then(res => {
-        console.log("res ", res);
-      
-        this.notifyDone();
+    }
 
-        // TODO: component did mount hook is grabbing the articles and setting articles in state. 
-        // Problem is when an assignment is made it should update the state on component did update 
-        // but we have a few states here to track and I am reusing a lot of components so will need
-        // to refetch the articles in order to reload state instead of reload. This will work for now.
-        setTimeout(function () { window.location.reload(); }, 1500);
-
-      })
-        .catch(err => {
-          console.log(err);
-        });
-    };
-    this.setState({
-      [`show${this.toTitleCase(type)}`]: false
-    });
+    if (eligibility.status === 'assigned') {
+      if (!_.isUndefined(eligibility[type])) {
+        return eligibility[type].email;
+      }
+    }
   }
 
   componentDidMount() {
-    const {
-      shortId
-    } = this.props.match.params;
+    const { shortId } = this.props.match.params;
 
     this.Article.getArticlesByBatch(shortId)
       .then(res => {
-        // console.log("res ", res);
         if (res.success) {
           this.setState({
             articles: res.data
@@ -90,10 +50,44 @@ class Articles extends React.Component {
       })
   }
 
+  assignment = (type, articleId) => {
+    const { user }  = this.props;
+    const { shortId } = this.props.match.params;
+      
+    const assignment = {
+      articleId,
+      stage: 'eligibility',
+      type,
+    };
+
+    assignment.user = {
+      _id: user.id,
+      email: user.email
+    }
+
+    this.Article.assign(assignment).then(res => {
+      if (res.success) {
+        this.notifyDone();
+        this.Article.getArticlesByBatch(shortId)
+          .then(res => {
+            if (res.success) {
+              this.setState({
+                articles: res.data
+              });
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      }
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
-    console.log("articles", this.state.articles);
-    const { user } = this.props;
-    
     return (
       <div className="box">
         <div className="table-responsive">
@@ -119,36 +113,28 @@ class Articles extends React.Component {
                     <td>{article.authors}</td>
                     <td>TBD</td>
                     <td>
-                      <Modal
-                        show={this.state.showJunior}
-                        handleClose={this.hideModal}
-                        user={user}
-                        stage='eligibility'
-                        articleId={article._id}
-                        type={'junior'}
-                      />
-                      <Assignment
-                        type="junior"
-                        showModal={this.showModal}
-                        toTitleCase={this.toTitleCase}
-                        article={article}
-                      />
+                      {
+                        this.showEmail(article, 'junior') ||
+                        <button
+                          className="md-btn md-flat mb-2 w-xs text-success"
+                          onClick={() => { 
+                            if (window.confirm('Are you sure you want to assign this article to your assigned quality appraisals list?')) this.assignment('junior', article._id) } 
+                          }>
+                          Assign
+                        </button>
+                      }
                     </td>
                     <td>
-                      <Modal
-                        show={this.state.showSenior}
-                        handleClose={this.hideModal}
-                        user={user}
-                        stage='eligibility'
-                        articleId={article._id}
-                        type={'senior'}
-                      />
-                      <Assignment
-                        type="senior"
-                        showModal={this.showModal}
-                        toTitleCase={this.toTitleCase}
-                        article={article}
-                      />
+                      {
+                        this.showEmail(article, 'senior') ||
+                        <button
+                          className="md-btn md-flat mb-2 w-xs text-success"
+                          onClick={() => {
+                            if (window.confirm('Are you sure you want to assign this article to your assigned quality appraisals list?')) this.assignment('senior', article._id) }
+                          }>
+                          Assign
+                        </button>
+                      }
                     </td>
                     <td>
                       <Link to={`/conflicts/${article.shortId}`}>
