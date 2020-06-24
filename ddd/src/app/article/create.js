7f21b62f24e376106a6dc1d3cda33fcb738aa91d
erobit <erobit3@gmail.com>
2020-06-23 21:31:21 -0400

    updated single article create to assign it to a batch of 1

--- ddd/src/app/article/create.js
+++ ddd/src/app/article/create.js
@@ -1,16 +1,34 @@
 const { hseArticle, sseArticle } = require("src/domain/article");
+const Batch = require("src/domain/batch");
+const ObjectID = require("mongodb").ObjectID;
+const shortid = require("shortid");
 
 /**
  * Article creation
  */
-module.exports = ({ articleRepository }) => {
-  const create = async article => {
+module.exports = ({ articleRepository, batchRepository }) => {
+  const create = async (article) => {
     try {
       if (!article.type || (article.type !== "sse" && article.type !== "hse")) {
         return {
-          error: "A valid article type is required"
+          error: "A valid article type is required",
         };
       }
+
+      const batch = {
+        type: article.type,
+        fileUrl: "",
+        referenceType: "system-generated",
+        source: "single-article-upload",
+        language: article.language,
+        uploaded: new Date(),
+        harvested: new Date(),
+      };
+
+      const newBatch = await batchRepository.create(Batch(batch));
+
+      article.batchId = new ObjectID(newBatch._id);
+      article.batchName = article.title;
 
       article.published = article.published
         ? new Date(article.published, 1, 1)
@@ -26,6 +44,6 @@ module.exports = ({ articleRepository }) => {
   };
 
   return {
-    create
+    create,
   };
 };
