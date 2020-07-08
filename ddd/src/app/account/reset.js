1064ff6005543a5e8d5003209a4dded70c65fd02
erobit <erobit3@gmail.com>
2020-07-07 22:49:17 -0400

    fixed dropzone component with dual submit buttons and bug with autoupload

--- ddd/src/app/account/reset.js
+++ ddd/src/app/account/reset.js
@@ -4,18 +4,18 @@ const emailer = require("./email");
  * Account reset password
  */
 module.exports = ({ config, userRepository, webToken, mailer, events }) => {
-  const send = async email => {
+  const send = async (email) => {
     try {
       const user = await userRepository.findByEmail(email);
       if (!user) {
         return {
-          error: "User not found"
+          error: "User not found",
         };
       }
 
       const data = {
         id: user.id,
-        email: user.email
+        email: user.email,
       };
 
       const resetEmail = emailer.reset({ config, webToken, mailer });
@@ -34,7 +34,7 @@ module.exports = ({ config, userRepository, webToken, mailer, events }) => {
       const user = await userRepository.findByEmail(email);
 
       const result = await userRepository.update(user._id, {
-        password: userRepository.encryptPassword(password)
+        password: userRepository.encryptPassword(password),
       });
 
       if (!result.ok) {
@@ -43,7 +43,7 @@ module.exports = ({ config, userRepository, webToken, mailer, events }) => {
 
       const data = {
         id: user.id,
-        email: user.email
+        email: user.email,
       };
 
       events.emit("account.reset", data);
@@ -54,6 +54,6 @@ module.exports = ({ config, userRepository, webToken, mailer, events }) => {
 
   return {
     send,
-    reset
+    reset,
   };
 };
