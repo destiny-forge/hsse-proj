0f51b10eb2a9595fd7f4a7ef4ad5f53bbb8eef1a
erobit <erobit3@gmail.com>
2020-09-30 20:16:35 -0400

    only progress article to In Progress once full assignment has been made

--- ui/src/views/Articles/Articles.js
+++ ui/src/views/Articles/Articles.js
@@ -60,14 +60,19 @@ class Articles extends React.Component {
       });
   }
 
-  assign = (role, articleId) => {
+  assign = (role, articleId, articleStage) => {
     const { user } = this.props;
     const { shortId, stage } = this.props.match.params;
+
+    const isFull =
+      !_.isUndefined(articleStage['junior']) ||
+      !_.isUndefined(articleStage['senior']);
 
     const assignment = {
       articleId,
       type: role,
       stage,
+      isFull,
     };
 
     assignment.user = {
@@ -202,7 +207,11 @@ class Articles extends React.Component {
                                   'Are you sure you want to assign this article to your assigned quality appraisals list?'
                                 )
                               )
-                                this.assign('junior', article._id);
+                                this.assign(
+                                  'junior',
+                                  article._id,
+                                  article.stages[stage]
+                                );
                             }}
                           >
                             Assign
@@ -223,7 +232,11 @@ class Articles extends React.Component {
                                   'Are you sure you want to assign this article to your assigned quality appraisals list?'
                                 )
                               )
-                                this.assign('senior', article._id);
+                                this.assign(
+                                  'senior',
+                                  article._id,
+                                  article.stages[stage]
+                                );
                             }}
                           >
                             Assign
