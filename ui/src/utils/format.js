09e9229ec8f7c617cc46ae96e80ba2607d593ba0
erobit <erobit3@gmail.com>
2021-01-21 01:07:32 -0500

    cleaned up expiry date format for user max search expiry date

--- ui/src/utils/format.js
+++ ui/src/utils/format.js
@@ -2,6 +2,11 @@ const getYear = (date) => {
   return new Date(Date.parse(date)).getFullYear();
 };
 
+const yearMonthDay = (date) => {
+  return date.split('T')[0];
+};
+
 module.exports = {
   getYear,
+  yearMonthDay,
 };
