f7533e38294344050166dc438bbea27b3dbd9c96
erobit <erobit3@gmail.com>
2021-05-12 19:44:25 -0400

    added type to the auth model

--- server/src/domain/auth/index.js
+++ server/src/domain/auth/index.js
@@ -1,8 +1,9 @@
-const t = require('tcomb');
+const t = require("tcomb");
 
 const Auth = t.struct({
+  type: t.String,
   email: t.String,
-  password: t.String
+  password: t.String,
 });
 
 module.exports = Auth;
