da714e0075836c717d01432257ac60606828cfc2
erobit <erobit3@gmail.com>
2021-05-12 09:31:37 -0400

    missed a Link

--- client/src/views/Accounts/ForgotPassword.js
+++ client/src/views/Accounts/ForgotPassword.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AuthService from '../../services/AuthService';
+import { Link } from 'react-router-dom';
 
 class ForgotPassword extends Component {
   constructor(props) {
