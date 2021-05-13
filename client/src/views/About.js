348e715e804f2df176692c731a87b86d2f261333
erobit <erobit3@gmail.com>
2021-05-13 10:33:05 -0400

    typo for site props

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -2,11 +2,15 @@ import Context from '../components/Context';
 import Markdown from '../components/Markdown';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Helmet } from 'react-helmet';
 
 const About = ({ site }) => {
   const className = `${site} app about layered-navigation`;
   return (
     <div className={className}>
+      <Helmet>
+        <title>About | {t('site_name')}</title>
+      </Helmet>
       <div className="layered-navigation-content">
         <Header />
         <div id="page-content">
